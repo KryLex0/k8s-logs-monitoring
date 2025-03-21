19671355190569a5acf00b964453c98816d5dfa2
Adam MKHININI <adam.mkh@hotmail.fr>
2025-03-21 15:34:12 +0100

    Correction password elasticsearch

--- vector-agent/elasticsearch_cmd.sh
+++ vector-agent/elasticsearch_cmd.sh
@@ -1,10 +1,10 @@
 # Delete index
-curl -X DELETE "https://10.0.5.13:31111/hdh-logs-2025-03-21" -u elastic:12VVU05Y1P8Bl09Jvj92VWUj --insecure
+curl -X DELETE "https://10.0.5.13:31111/hdh-logs-2025-03-21" -u elastic:eSFZ387hx0a94m96B7LcH0AF --insecure
 
 # Update index mapping
 # (update mapping.properties.message.properties.stack.type to nested)
 curl -X PUT -X PUT "https://10.0.5.13:31111/hdh-logs-2025-03-21/_mapping" \
-      -u elastic:12VVU05Y1P8Bl09Jvj92VWUj --insecure \
+      -u elastic:eSFZ387hx0a94m96B7LcH0AF --insecure \
       -H 'Content-Type: application/json' -d'
       {
         "properties": {
@@ -22,10 +22,10 @@ curl -X PUT -X PUT "https://10.0.5.13:31111/hdh-logs-2025-03-21/_mapping" \
 # https://appariements.pre-prod-hdh.fr/connexion
 
 # Get indices
-curl -X GET "https://10.0.5.13:31111/_cat/indices?v" -u elastic:12VVU05Y1P8Bl09Jvj92VWUj --insecure
+curl -X GET "https://10.0.5.13:31111/_cat/indices?v" -u elastic:eSFZ387hx0a94m96B7LcH0AF --insecure
 
 # Get index mapping
-curl -X GET "https://10.0.5.13:31111/hdh-logs-2025-03-21/_mapping?pretty"  -u elastic:12VVU05Y1P8Bl09Jvj92VWUj --insecure
+curl -X GET "https://10.0.5.13:31111/hdh-logs-2025-03-21/_mapping?pretty"  -u elastic:eSFZ387hx0a94m96B7LcH0AF --insecure
 
 
       
