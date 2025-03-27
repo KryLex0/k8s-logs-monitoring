03b9836c54987b10a60dd206ab5c43b96aa5d17c
Adam MKHININI <adam.mkh@hotmail.fr>
2025-03-27 11:13:20 +0100

    Correction of vector fleet and elastalert2 rule

--- vector-agent/elasticsearch_cmd.sh
+++ vector-agent/elasticsearch_cmd.sh
@@ -1,9 +1,9 @@
 # Delete index
-curl -X DELETE "https://10.0.5.13:31111/k8s-logs-2025-03-21" -u elastic:eSFZ387hx0a94m96B7LcH0AF --insecure
+curl -X DELETE "https://10.0.5.13:31111/hdh-logs-2025-03-21" -u elastic:eSFZ387hx0a94m96B7LcH0AF --insecure
 
 # Update index mapping
 # (update mapping.properties.message.properties.stack.type to nested)
-curl -X PUT -X PUT "https://10.0.5.13:31111/k8s-logs-2025-03-21/_mapping" \
+curl -X PUT -X PUT "https://10.0.5.13:31111/hdh-logs-2025-03-21/_mapping" \
       -u elastic:eSFZ387hx0a94m96B7LcH0AF --insecure \
       -H 'Content-Type: application/json' -d'
       {
@@ -25,7 +25,7 @@ curl -X PUT -X PUT "https://10.0.5.13:31111/k8s-logs-2025-03-21/_mapping" \
 curl -X GET "https://10.0.5.13:31111/_cat/indices?v" -u elastic:eSFZ387hx0a94m96B7LcH0AF --insecure
 
 # Get index mapping
-curl -X GET "https://10.0.5.13:31111/k8s-logs-2025-03-21/_mapping?pretty"  -u elastic:eSFZ387hx0a94m96B7LcH0AF --insecure
+curl -X GET "https://10.0.5.13:31111/hdh-logs-2025-03-21/_mapping?pretty"  -u elastic:eSFZ387hx0a94m96B7LcH0AF --insecure
 
 
       
